b58f39968fe8e3d124fb5093544820286ee4ea1e
VineeS <shuklavinee@gmail.com>
2024-06-22 12:53:23 -0400

    fast API main.py

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 from fastapi import FastAPI
-from myLib.logic import wiki
+import uvicorn
+from myLib.logic import wiki as wikilogic
+from myLib.logic import search_wiki
 
-res = wiki()
-print(res)
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Wiki API call. Call /search or /wiki"}
+
+@app.get("/search/{value}")
+async def search(value: str):
+    """Page to search"""
+
+    result = search_wiki(value)
+    return {"result": result}
+
+@app.get("/wiki/{name}")
+async def wiki_search(name: str):
+    """Content to Search"""
+    res = wikilogic(name)
+    return {"res": res}
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
